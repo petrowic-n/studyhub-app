ad513df7f63b5b0bf515e953a800c199ff6c0bae
petrowic-n <nemanjapetrovic358@gmail.com>
2025-12-04 09:59:52 +0100

    section: study, tabbed component added

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/mobile-nav.css";
 import "../styles/components/hero.css";
+import "../styles/components/study.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
 import siteAnimations from "./utils/animations";
+import tabbedComponent from "./utils/tabbed-component";
 mobileNav();
 siteAnimations();
+tabbedComponent();
---
+++ src/utils/tabbed-component.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const tabbedComponent = () => {
+  const tabButtons = document.querySelectorAll(".study__btn");
+  const tabButtonsContainer = document.querySelector(".study__tab-buttons");
+  const tabContent = document.querySelectorAll(".study__content");
+
+  tabButtonsContainer.addEventListener("click", function (e) {
+    const studyBtnClicked = e.target.closest(".study__btn");
+
+    if (!studyBtnClicked) return;
+
+    tabButtons.forEach((t) => t.classList.remove("study__btn--active"));
+    studyBtnClicked.classList.add("study__btn--active");
+
+    tabContent.forEach((tabContent) =>
+      tabContent.classList.remove("study__content--active")
+    );
+
+    document
+      .querySelector(`.study__content--${studyBtnClicked.dataset.tab}`)
+      .classList.add("study__content--active");
+  });
+};
+
+export default tabbedComponent;
